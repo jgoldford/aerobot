344c37d58c2894f0d1da05fd6506da1cf16a5819
Joshua E. Goldford <goldford@hpc-23-23.cm.cluster>
2023-02-07 23:08:32 -0800

    added a function to load validation data

--- aerobot/.ipynb_checkpoints/io-checkpoint.py
+++ aerobot/.ipynb_checkpoints/io-checkpoint.py
@@ -6,7 +6,6 @@ import subprocess as sb
 asset_path,filename = os.path.split(os.path.abspath(__file__))
 asset_path = asset_path + '/assets'
 
-
 def download_training_data():
     # donwload the training data from google cloud
 
@@ -50,3 +49,20 @@ def load_ko2ec():
 def load_oxygen_kos():
     return pd.read_csv(asset_path + "/mappings/ko_groups.oxygenAssociated.07Feb2023",index_col=0)
 
+def load_validation_data(feature_type="KO"):
+    output = {"features":[],"labels":[]}
+    feature_path = asset_path + "/validation/features/"
+    labels = pd.read_csv(asset_path+"/validation/labels/Jablonska_Labels.07Feb2023.csv",index_col=0)
+    output["labels"] = labels
+    if feature_type == "KO":
+        feature_matrix = pd.read_csv(feature_path + "Jablonska_FS.KOCounts.07Feb2023.csv",index_col=0)
+    elif feature_type == "embedding.genome":
+        feature_matrix = pd.read_csv(feature_path + "Jablonska_FS.WGE.07Feb2023.csv",index_col=0)
+    elif feature_type == "embedding.geneset.oxygen":
+        feature_matrix = pd.read_csv(feature_path + "Jablonska_FS.OGSE.07Feb2023.csv",index_col=0)
+    elif feature_type == "metadata":
+        feature_matrix = pd.read_csv(feature_path + "Jablonska_FS.AF.07Feb2023.csv",index_col=0)
+    else:
+        raise Exception("please use KO (all KO counts), WGE (whole genome embedding), or OGSE (oxygen gene set embedding)")
+    output["features"] = feature_matrix
+    return output
--- aerobot/.ipynb_checkpoints/lib-checkpoint.py
+++ aerobot/.ipynb_checkpoints/lib-checkpoint.py
@@ -1,6 +1,28 @@
 import pandas as pd
 import os
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
 
 asset_path,filename = os.path.split(os.path.abspath(__file__))
 asset_path = asset_path + '/assets'
 
+# define asset path
+asset_path,filename = os.path.split(os.path.abspath(__file__))
+asset_path = asset_path + '/assets'
+
+
+def run_pca(X,n_components=2,normalize=True):
+	results = {}
+	pca = PCA(n_components=n_components)
+	if normalize:
+		Xn = StandardScaler().fit_transform(X.values)
+	else:
+		Xn = X.values
+	Xpca = pca.fit_transform(Xn)
+
+	labels = ["PC{x} ({y}%)".format(x=x,y=round(y*100,2)) for x,y in list(zip(range(1,n_components+1),pca.explained_variance_ratio_))]
+	pdf = pd.DataFrame(Xpca,index=X.index,columns =labels)
+	results["pdf"] = pdf
+	results["pca"] = pca
+	return results
