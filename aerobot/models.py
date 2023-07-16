26b6a16b2c64cb68a6d0166ed5c453c96094fe3e
Joshua Goldford <goldford.joshua@gmail.com>
2023-07-16 13:42:47 -0700

    added new logistic regression model to repo

--- aerobot/models.py
+++ aerobot/models.py
@@ -1,10 +1,11 @@
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegressionCV
+from sklearn.metrics import balanced_accuracy_score, confusion_matrix
 from sklearn import metrics
 import joblib
 
 class LogisticClassifier:
-    def __init__(self, cs=10, cv=5, random_state=42,max_iter=1e4):
+    def __init__(self, cs=10, cv=5, random_state=42,max_iter=int(1e4)):
         self.classifier = LogisticRegressionCV(Cs=cs, cv=cv, random_state=random_state,max_iter=max_iter)
         self.scaler = StandardScaler()
         
@@ -19,6 +20,17 @@ class LogisticClassifier:
     def score(self, X, y):
         X = self.scaler.transform(X)
         return self.classifier.score(X, y)
+    
+    def balanced_accuracy(self, X, y):
+        X = self.scaler.transform(X)
+        y_pred = self.classifier.predict(X)
+        return balanced_accuracy_score(y, y_pred)
+
+    def confusion_matrix(self, X, y):
+        X = self.scaler.transform(X)
+        y_pred = self.classifier.predict(X)
+        return confusion_matrix(y, y_pred)
+
 
     def save(self, filename):
         joblib.dump((self.classifier, self.scaler), filename)
