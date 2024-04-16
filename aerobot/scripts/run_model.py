49a319161adf5d50612da61b232989250d76538e
Philippa Richter <pips.richter@gmail.com>
2024-04-15 21:03:06 -0700

    Updating scripts and cleaning up dataset construction

--- aerobot/scripts/run_model.py
+++ aerobot/scripts/run_model.py
@@ -3,76 +3,17 @@ import pandas as pd
 import numpy as np
 import tqdm
 from aerobot.io import ASSET_PATH, FEATURE_TYPES, FEATURE_SUBTYPES, NumpyEncoder
-from aerobot.datasets import load_datasets
-from sklearn.linear_model import LogisticRegression
-from aerobot.models import GeneralClassifier, Nonlinear
+from aerobot.dataset import dataset_load_training_validation
+from aerobot.models import train_nonlinear, train_logistic, evaluate 
 # from joblib import Parallel, delayed
 from os import path
 import argparse
 from typing import Dict, NoReturn
+import time
 import pickle
 import json
 
 
-def train_logistic(X:np.ndarray, y:np.ndarray, params:Dict={'penalty':'l2', 'C':100, 'max_iter':10000}) -> GeneralClassifier:
-    '''Train a LogisticRegression-based classifier.
-
-    :param X: A numpy array containing the training features.
-    :param y: A numpy array containing the training labels.
-    :param params: A dictionary of keyword arguments to pass into the classifier initialization function.
-    :return: A trained instance of a GeneralClassifier based on logistic regression.
-    '''
-    model = GeneralClassifier(model_class=LogisticRegression, params=params) # Instantiate a classifier.
-    model.fit(X, y)
-    return model
-
-
-def train_nonlinear(X:np.ndarray, y:np.ndarray, X_val:np.ndarray, y_val:np.ndarray, params:Dict=None) -> GeneralClassifier:
-    '''Train a Nonlinear-based classifier.
-
-    :param X: A numpy array containing the training features.
-    :param y: A numpy array containing the training labels.
-    :param X_val: A numpy array containing the validation features.
-    :param y_val: A numpy array containing the validation labels. 
-    :param params: A dictionary of keyword arguments to pass into the classifier initialization function.
-    :return: A trained instance of a GeneralClassifier based on a nonlinear neural network.
-    '''
-    model = GeneralClassifier(model_class=Nonlinear, params=params) # Instantiate a classifier.
-    model.fit(X, y, X_val=X_val, y_val=y_val)
-    return model
-
-
-def evaluate(model:GeneralClassifier, X:np.ndarray, y:np.ndarray, X_val:np.ndarray, y_val:np.ndarray) -> Dict:
-    '''Evaluate a trained GeneralClassifier using the training and test data.
-
-    :param model: The trained GeneralClassifier to evaluate.
-    :param X: A numpy array containing the training features.
-    :param y: A numpy array containing the training labels.
-    :param X_val: A numpy array containing the validation features.
-    :param y_val: A numpy array containing the validation labels. 
-    :return: A dictionary containing various evaluation metrics for the trained classifier on the training
-        and validation data.
-    '''
-    results = dict()
-
-    results['training_acc'] = model.balanced_accuracy(X, y)
-    results['validation_acc'] = model.balanced_accuracy(X_val, y_val)
-    results['classes'] = model.classifier.classes_
-    results['confusion_matrix'] = model.confusion_matrix(X_val, y_val).ravel()
-
-    if isinstance(model.classifier, LogisticRegression): # Only applies if the model used LogisticRegression.
-        n_iter = model.classifier.n_iter_[0]
-        results['n_iter'] = n_iter
-        results['converged'] = n_iter < model.classifier.max_iter
-
-    if isinstance(model.classifier, Nonlinear): # If the underlying model is Nonlinear...
-        # Save some information for plotting training curves.
-        results['training_losses'] = model.classifier.train_losses
-        results['validation_losses'] = model.classifier.val_losses
-        results['training_accs'] = model.classifier.train_accs
-        results['validation_accs'] = model.classifier.val_accs
-    
-    return results
 
 
 def print_summary(results:Dict) -> NoReturn:
@@ -103,9 +44,10 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
     check_args(args)
+    t1 = time.perf_counter()
 
     # Load the cleaned-up training and validation datasets.
-    training_dataset, validation_dataset = load_datasets(args.feature_type, binary=args.binary)
+    training_dataset, validation_dataset = dataset_load_training_validation(args.feature_type, binary=args.binary, to_numpy=True)
     # Unpack the numpy arrays stored in the dictionary. 
     X, y, X_val, y_val = training_dataset['features'], training_dataset['labels'], validation_dataset['features'], validation_dataset['labels']
 
@@ -134,27 +76,7 @@ if __name__ == '__main__':
     elif args.output_format == 'json': # If specified, save results in a json file.
         with open(args.out, 'w') as f:
             json.dump(results, f, cls=NumpyEncoder)
+    
+    t2 = time.perf_counter()
+    print(f'\nModel run complete in {np.round(t2 - t1, 2)} seconds.')
 
-
-
-# pretty_feature_names = {
-#     'KO': 'all gene families',
-#     'embedding.genome': 'genome embedding',
-#     'embedding.geneset.oxygen': '5 gene set',
-#     'metadata.number_of_genes': 'number of genes',
-#     'metadata.oxygen_genes': 'O$_2$ gene count',
-#     'metadata.pct_oxygen_genes': 'O$_2$ gene percent',
-#     'aa_1mer': 'amino acid counts',
-#     'aa_2mer': 'amino acid dimers',
-#     'aa_3mer': 'amino acid trimers',
-#     'chemical': 'chemical features',
-#     'nt_1mer': 'nucleotide counts',
-#     'nt_2mer': 'nucleotide dimers',
-#     'nt_3mer': 'nucleotide trimers',
-#     'nt_4mer': 'nucleotide 4-mers',
-#     'nt_5mer': 'nucleotide 5-mers',
-#     'cds_1mer': 'CDS nucleotide counts',
-#     'cds_2mer': 'CDS nucleotide dimers',
-#     'cds_3mer': 'CDS nucleotide trimers',
-#     'cds_4mer': 'CDS nucleotide 4-mers',
-#     'cds_5mer': 'CDS nucleotide 5-mers'}
