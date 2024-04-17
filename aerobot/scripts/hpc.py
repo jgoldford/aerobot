18840daed66d7867904330c24cb814ce0242b1d0
Philippa Richter <pips.richter@gmail.com>
2024-04-16 23:02:44 -0700

    Fixing hpc.py

--- aerobot/scripts/hpc.py
+++ aerobot/scripts/hpc.py
@@ -6,21 +6,21 @@ import os
 
 # TODO: Might be worth supporting command-line arguments for this.
 
-# Get the path within aerobot where all the Python scripts are stored. 
-SCRIPTS_PATH, _ = os.path.split(os.path.abspath(__file__))
-RUN_MODEL = os.path.join(SCRIPTS_PATH, RUN_MODEL)
-OUTPUT_DIR = '~'
-OUTPUT_FORMAT = 'json'
-TIME = '02:00:00' # Time for slurm job. 
-MEM = '20GB' # Memory for slurm job. 
-
 
 if __name__ == '__main__':
+
+    # Get the path within aerobot where all the Python scripts are stored. 
+    SCRIPTS_PATH, _ = os.path.split(os.path.abspath(__file__))
+    RUN = os.path.join(SCRIPTS_PATH, 'run.py')
+    OUTPUT_DIR = '~'
+    OUTPUT_FORMAT = 'json'
+    TIME = '02:00:00' # Time for slurm job. 
+    MEM = '20GB' # Memory for slurm job. 
 
     for model_class in ['logistic', 'nonlinear']:
         for feature_type in FEATURE_TYPES:
             output_filename = f'run_model_results_{model_class}_{feature_type}.' + OUTPUT_FORMAT
             output_path = os.path.join(OUTPUT_DIR, output_filename)
-            cmd = f'{RUN_MODEL} {model_class} --feature-type {feature_type} --output-format {OUTPUT_FORMAT} -o {output_path}'
+            cmd = f'{RUN} {model_class} --feature-type {feature_type} --output-format {OUTPUT_FORMAT} -o {output_path}'
             # Submit the slurm job. 
             subprocess.run(f'sbatch --wrap "{cmd}" -N 1 --time {TIME} --mem {MEM}', check=True, shell=True)
